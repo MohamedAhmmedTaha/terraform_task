8657ab4b77a5724de2cb4fa6b2760457edf28b38
MohammedAhmedTaha <mohamed.a.taha2222@gmail.com>
2025-03-22 14:35:10 +0200

    state file modification

--- providor.tf
+++ providor.tf
@@ -1,13 +1,18 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-
-#   required_version = ">= 1.11.1"
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.92.0"
+    }
+  }
+  backend "s3" {
+    bucket = "my-bucket-task-22"
+    key = "my-state-file"
+    region = "us-east-1"
+    
+  }
+ 
+}
 
 provider "aws" {
   region = "us-east-1"
